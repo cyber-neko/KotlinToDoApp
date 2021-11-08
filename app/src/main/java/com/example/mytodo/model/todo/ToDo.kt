9d6ffbcf0a5ee09bd18f900dcc9b42dd98d05189
Cyber Neko <firexneko7@gmail.com>
2021-11-08 22:21:47 +0900

    ToDoにPercelableを継承

--- app/src/main/java/com/example/mytodo/model/todo/ToDo.kt
+++ app/src/main/java/com/example/mytodo/model/todo/ToDo.kt
@@ -1,10 +1,13 @@
 package com.example.mytodo.model.todo
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
 
 //@Entityでroomが対応するテーブルを作成(テーブル1行分(フィールド)のデータクラスを作成)
 @Entity
+@Parcelize
 data class ToDo(
     //各プロパティはテーブルのフィールド(列)
     @PrimaryKey(autoGenerate = true)
@@ -13,4 +16,4 @@ data class ToDo(
     val detail: String,
     val created: Long,
     val modified: Long
-)
+) : Parcelable //オブジェクト内部のデータを一旦Parcelという箱に避難させ後から中身を取り出してオブジェクトを復元する
